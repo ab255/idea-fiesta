6523a1e3afc549cc9c0f5f3f22af9aff0f32dce6
Adam Bernard <ab255@me.com>
2016-07-19 14:42:23 -0600

    ideas commented in

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// create a function that stores added title and body, function that makes dynamic element ('li')
+// function that removes li
+// function that changes quality
+// function that searches with li's
+// function to be able to edit li's
+// function that replaces edited li
+// function to local store and remove li's
+// Create new function or add to other function, to call on stored li's
+// functions for buttons that will refrence previous buttons
