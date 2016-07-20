95d9c46c43baa810d679425953a3eac6a0253e08
Ben Godfrey <redzone41@gmail.com>
2016-07-19 20:08:30 -0600

    Add buttons to li's and add functionality for delte button.

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ saveButton.on('click', function(){
   $('ul').append(
     '<li>' +
       '<h2 class="idea-title">' + titleInput.val() + '</h2>' +
-      '<img>' +
+      '<input class="delete-button" type="image" src="images/delete.svg" height="20" width="20"' +
       '<p class="idea-description">' + descriptionInput.val() + '</p>' +
-      '<img>' +
-      '<img>' +
-      '<p class="ranking-value">' + 'ranking: swill' + '</p>' +
+      '<input class="upvote-button" type="image" src="images/upvote.svg" height="20" width="20">' +
+      '<input class="downvote-button" type="image" src="images/downvote.svg" height="20" width="20"' +
+      '<p class="quality-value">' + 'ranking: swill' + '</p>' +
     '</li>');
 });
 
+
 // function that removes li
+
+$('ul').on('click', '.delete-button', function() {
+  $(this).parent().remove('li');
+});
+
+
 // function that changes quality
+
+
 // function that searches with li's
 // function to be able to edit li's
 // function that replaces edited li
