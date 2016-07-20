a80b32490098fec9eb51530b435bf9c1d5894bbf
Ben Godfrey <redzone41@gmail.com>
2016-07-19 19:21:06 -0600

    Add scripts for jquery and javascript to html and create save button function.

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // create a function that stores added title and body, function that makes dynamic element ('li' li's need to have a h1, p , [delete, uipvote, downvote] buttons, quality)
 var titleInput = $('.title-input-js');
 var descriptionInput = $('.description-input-js');
-var createIdea = $('ul').append(
-  '<li>' +
-    '<h2>' + titleInput.val() + '</h2>' +
-    '<img>' +
-    '<p>' + descriptionInput.val() + '</p>' +
-    '<img>' +
-    '<img>' +
-    '<p>' + 'ranking: ' + ideaRanking() + '</p>' +
-  '</li>'
-);
+var saveButton = $('.save-button');
+
+
+saveButton.on('click', function(){
+  $('ul').append(
+    '<li>' +
+      '<h2 class="idea-title">' + titleInput.val() + '</h2>' +
+      '<img>' +
+      '<p class="idea-description">' + descriptionInput.val() + '</p>' +
+      '<img>' +
+      '<img>' +
+      '<p class="ranking-value">' + 'ranking: swill' + '</p>' +
+    '</li>');
+});
+
 // function that removes li
 // function that changes quality
 // function that searches with li's
